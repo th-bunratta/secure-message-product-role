98587afdee1bebbada227b8756990d1d82007cf1
Th <tharathorn.bu@ku.th>
2021-05-07 23:20:43 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+
+- Homework 1: PBKDF2
+- [x] Homework 2: bcrypt
+
+  Modify the project class to satisfy OWASP’s bcrypt security requirement (work factor should be at least 13)
+  
+- Homework 3: add Google SSO
---
+++ src/main/java/ku/message/service/UserService.java
@@ -0,0 +1,49 @@
+
+
+package ku.message.service;
+
+import ku.message.model.User;
+import ku.message.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Autowired
+    private HashService hashService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public boolean isUsernameAvailable(String username) {
+        return repository.findByUsername(username) == null;
+    }
+
+    public int createUser(User user) {
+        User newUser = new User();
+        newUser.setFirstName(user.getFirstName());
+        newUser.setLastName(user.getLastName());
+        newUser.setUsername(user.getUsername());
+        newUser.setRole(user.getRole());
+
+  //      String salt = hashService.getSalt();
+        String hashedPassword = passwordEncoder.encode(user.getPassword());
+                //hashService.getHashedValue(user.getPassword(), salt);
+
+      //  newUser.setSalt(salt);
+        newUser.setPassword(hashedPassword);
+
+        repository.save(newUser);
+        return 1;
+    }
+
+    public User getUser(String username) {
+        return repository.findByUsername(username);
+    }
+}
+
